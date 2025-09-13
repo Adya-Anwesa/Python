a541b525c02ea6f87b4983cec3badd08bead2159
Adya-Anwesa <adyaanwesa@gmail.com>
2025-09-13 01:05:04 -0700

    Updated code for changing parameters in the plot

--- Pre-CPE.py
+++ Pre-CPE.py
@@ -10,14 +10,14 @@ Current= data["Cur"]
 
 # Step 3c: Plot the CV curve
 plt.figure(figsize=(6,4))
-plt.plot(Potential, Current, color='red', linewidth=2)
-plt.title("Pre-CPE")
-plt.xlabel("Potential (V)")
-plt.ylabel("Current density (mA/cm2)")
-plt.grid(True)
+plt.plot(Potential, Current, color='red', linewidth=2, label="Pre Electrolysis")
+plt.legend(loc='upper left',
+fontsize=12,   
+ frameon=False 
+)
+plt.xlabel("Potential (V $\t{vs.}$ Fc$^{0/+}$)")
+plt.ylabel("J (mA/cm$^2$)")
 
+plt.tight_layout()  # Automatically adjusts spacing so nothing is cut off
 # Step 3d: Save the plot as an image
 plt.savefig("Pre-CPE.png")
-
-# Step 3e: Show the plot
-plt.show()
